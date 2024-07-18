5b0a0f10fe8267ee958e39c30d58d7227ac749cf
soyebkingtide <soyeb@kingtide.com>
2024-07-18 11:41:28 +0530

    sad

--- assets/custom.js
+++ assets/custom.js
@@ -1,16 +1,24 @@
+// Select all elements with class "card--media"
 const cardMedia = document.querySelectorAll(".card--media");
 
+// Select all elements with class "controlVd"
 const videoElem = document.querySelectorAll(".controlVd");
 
+// Iterate over each cardMedia element
 cardMedia.forEach((VD, index) => {
+  // Add event listener for mouseenter
   VD.addEventListener("mouseenter", function () {
     console.log(videoElem[index]);
-    videoElem[index].play();
+    if (videoElem[index]) {
+      videoElem[index].play(); // Play the corresponding video element
+    }
   });
 
+  // Add event listener for mouseout
   VD.addEventListener("mouseout", function () {
     console.log(videoElem[index]);
-    console.log(this);
-    videoElem[index].pause();
+    if (videoElem[index]) {
+      videoElem[index].pause(); // Pause the corresponding video element
+    }
   });
 });
