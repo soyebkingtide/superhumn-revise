eb76d71ba25ad97963edaf000e2e1d12a9ecff3b
soyebkingtide <soyeb@kingtide.com>
2024-07-18 11:26:03 +0530

    video js

--- assets/custom.js
+++ assets/custom.js
@@ -1,1 +1,13 @@
-console.log("Working....");
+const vd = document.querySelectorAll(".controlVd");
+
+vd.forEach(
+  (controlVD = () => {
+    this.addEventListener("mouseenter", function () {
+      this.play();
+    });
+
+    this.addEventListener("mouseout", function () {
+      this.pause();
+    });
+  })
+);
