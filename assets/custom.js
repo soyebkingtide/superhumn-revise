9c2d33e8ff7cf60a4f420722cb248bceae97e481
soyebkingtide <soyeb@kingtide.com>
2024-08-07 21:35:26 +0530

    sdaf

--- assets/custom.js
+++ assets/custom.js
@@ -1,18 +1,47 @@
-const cardMedia = document.querySelectorAll(".video-block");
+document.addEventListener("DOMContentLoaded", function () {
+  const cardMedia = document.querySelectorAll(".video-block");
 
-cardMedia.forEach((VD, index) => {
-  const videoElem = document.querySelectorAll(".controlVd");
+  cardMedia.forEach((VD, index) => {
+    const videoElem = document.querySelectorAll(".controlVd");
 
-  VD.addEventListener("mouseenter", function () {
-    // console.log(videoElem[index]);
-    videoElem[index].play();
+    VD.addEventListener("mouseenter", function () {
+      // console.log(videoElem[index]);
+      videoElem[index].play();
+    });
+
+    VD.addEventListener("mouseout", function () {
+      // console.log(videoElem[index]);
+      // console.log(this);
+      videoElem[index].pause();
+    });
   });
 
-  VD.addEventListener("mouseout", function () {
-    // console.log(videoElem[index]);
-    // console.log(this);
-    videoElem[index].pause();
-  });
+  // script.js
+
+  // starter animation
+
+  function startAni(elements) {
+    var featureImage = document.querySelector(elements);
+
+    function checkVisibility() {
+      var rect = featureImage.getBoundingClientRect();
+      var windowHeight = window.innerHeight;
+
+      // Check if the element is fully within the viewport
+      if (rect.top >= 0 && rect.bottom <= windowHeight) {
+        featureImage.classList.add("visible");
+      } else {
+        featureImage.classList.remove("visible");
+      }
+    }
+
+    // Initial check
+    checkVisibility();
+
+    // Check visibility on scroll
+    window.addEventListener("scroll", checkVisibility);
+  }
+  startAni(".feature-image");
 });
 
 jQuery(function ($) {
@@ -30,3 +59,5 @@ jQuery(function ($) {
     pauseOnHover: true,
   });
 });
+
+// .feature-img
