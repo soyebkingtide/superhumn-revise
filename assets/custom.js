802a685d2577e2dcae0025c0158e1338f87dc716
soyebkingtide <soyeb@kingtide.com>
2024-08-22 21:02:40 +0530

    sdf

--- assets/custom.js
+++ assets/custom.js
@@ -48,7 +48,7 @@ function adjustPosTopImage() {
   if (screenSize < 992) {
     floatingCharacters.style.top = `calc(${topPosition}px)`;
   } else {
-    floatingCharacters.style.top = `calc(${topPosition}px + 3%)`;
+    floatingCharacters.style.top = `calc(${topPosition}px + 20%)`;
   }
 
   console.log("top position: " + floatingCharacters.style.top);
