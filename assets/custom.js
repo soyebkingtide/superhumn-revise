0c902363118e7c320690b66299252bbc51b80633
soyebkingtide <soyeb@kingtide.com>
2024-09-23 23:47:27 +0530

    test

--- assets/custom.js
+++ assets/custom.js
@@ -77,6 +77,41 @@ jQuery(function ($) {
   });
 });
 
+// Testimonials Slider
+
+$(".slider-testimonials").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 3600,
+  autoplaySpeed: 0, // Set to 0 for continuous sliding
+  cssEase: "linear", // For smooth, continuous movement
+  infinite: true, // Allows continuous looping
+  dots: false,
+  arrows: false,
+
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
 // Global function to check visibility and apply a class
 function checkVisibilityAndToggleClass(selector, className) {
   // Select the element based on the provided selector
